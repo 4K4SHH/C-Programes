5f3fc19ec3b812e729ace8329de2ff18e09a4f0d
AKASH HARIDAS <81566150+AKASH-224@users.noreply.github.com>
2021-04-24 15:17:08 +0530

    Add files via upload

---
+++ distance.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  float l1,l2,g1,g2,d;
+  printf("Enter the latitude and logitude of first place:");
+  scanf("%f%f",&l1,&g1);
+  printf("Enter the latitude and logitude of second place:");
+  scanf("%f%f",&l2,&g2);
+  l1=l1*(3.14/180);
+  l2=l2*(3.14/180);
+  g1=g1*(3.14/180);
+  g2=g2*(3.14/180);
+  d=3963*acos((sin(l1)*sin(l2))+(cos(l1)*cos(l2)*cos(g2-g1)));
+  printf("Distance = %f",d);
+}
