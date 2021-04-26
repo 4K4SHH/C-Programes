ff8913c08463c4a7a0aa1cc0b9d06c8822cde3e6
AKASH HARIDAS <81566150+AKASH-224@users.noreply.github.com>
2021-04-26 14:24:30 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int n;
+  printf("Enter a Number: ");
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+    printf("Even Number");
+  }
+  else
+  {
+    printf("odd number");
+  }
+}
